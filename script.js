e4a65388505d08ccee6dc59cc99c184c297853e7
DvHanslei <aihanslei@gmail.com>
2026-05-28 23:17:22 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
-canvas.width = 600;
-canvas.height = 600;
+canvas.width = 700;
+canvas.height = 700;
 
 const levelText = document.getElementById("level");
 const timeText = document.getElementById("time");
